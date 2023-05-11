5f887512e5ea09c54f715997ce9e10072a132bbd
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2023-05-11 22:00:06 +0900

    feat: Execute lexical analysis

--- cli/main.c
+++ cli/main.c
@@ -1,7 +1,32 @@
-#include "../src/includes.h"
-#include "../src/macros.h"
+#include "../src/types.h"
 
 int main(int argc, char **argv) {
-  fputs("0x80\n", stdout);
+  if (argc != 2) {
+    fputs("./bin/x source\n", stderr);
+    exit(EXIT_FAILURE);
+  }
+
+  FILE *source_file = fopen(argv[1], "r");
+
+  if (source_file == NULL) {
+    perror("fopen");
+    exit(EXIT_FAILURE);
+  }
+
+  Token *tokens = malloc(MAX_TOKENS * sizeof(Token));
+
+  lexer(source_file, tokens);
+
+  Token token;
+
+  int p = 0;
+
+  do {
+    token = tokens[p++];
+    printf("%-10s %3d %d\n", token.text, token.type, token.value);
+  } while (strncmp("", token.text, 1) != 0);
+
+  free(tokens);
+
   return 0;
 }
