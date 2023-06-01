559c7f98044382ac7c5941fc7d8ef86f6f0486be
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2023-06-01 17:21:37 +0900

    feat: Symbol Table

--- src/types.h
+++ src/types.h
@@ -37,11 +37,49 @@ typedef enum {
   END_LIST
 } TokenType;
 
+typedef enum {
+  LITERAL_ID,
+  VARIABLE_ID,
+  FUNCTION_ID,
+  PROTOTYPE_ID,
+  ARGUMENT_ID
+} SymbolType;
+
 typedef struct {
   TokenType type;
   char text[SIZE_OF_TEXT + 1];
   int value;
 } Token;
+
+typedef enum { NON_T, VOID_T, INT_T } DataType;
+
+/**
+ * Text Segment
+ * -------------
+ * Data Segment
+ * - PPDA (Per Process Data Area)
+ * - Data Area
+ *  - Static Area
+ *  - Heap Area
+ * - Stack Area
+ */
+typedef enum {
+  GLOBAL, // Data Area
+  LOCAL   // Stack Area
+} Memory;
+
+typedef struct {
+  char *symbol;
+  SymbolType symbol_type;
+  DataType type;
+  int length;
+  Memory memory;
+  char argc;
+  int address;
+} Symbol;
+
+Symbol symbol_table[SIZE_OF_SYMBOL_TABLE];
+Symbol tmp_symbol;
 
 extern void lexer(FILE *source_file, Token *tokens);
 extern void dump_lexer(FILE *source_file);
